2645295f1d887e0f63118a6edb90a11eb09fe759
Tahmina <233046060+tahminaakter163001-dev@users.noreply.github.com>
2026-09-18 19:27:51 +0600

    Add user model

--- src/main/java/com/example/projectmvc/model/User.java
+++ src/main/java/com/example/projectmvc/model/User.java
@@ -1,4 +1,20 @@
 package com.example.projectmvc.model;
 
 public class User {
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
 }
