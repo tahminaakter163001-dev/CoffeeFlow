9b50f9ac48cfdf5af1fca6f66b990afdf352b5fb
Tahmina <233046060+tahminaakter163001-dev@users.noreply.github.com>
2026-09-18 20:59:43 +0600

    Make login page responsive

---
+++ src/main/java/com/example/projectmvc/Main.java
@@ -0,0 +1,26 @@
+package com.example.projectmvc;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        FXMLLoader fxmlLoader =
+                new FXMLLoader(Main.class.getResource("/com/example/projectmvc/view/login-view.fxml"));
+
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle("CoffeeFlow");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,5 @@
 module com.example.projectmvc {
+
     requires javafx.controls;
     requires javafx.fxml;
 
@@ -6,5 +7,8 @@ module com.example.projectmvc {
     requires org.kordamp.bootstrapfx.core;
 
     opens com.example.projectmvc to javafx.fxml;
+    opens com.example.projectmvc.controller to javafx.fxml;
+
     exports com.example.projectmvc;
 }
+
