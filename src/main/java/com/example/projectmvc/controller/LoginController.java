3dc3623ce451be000e2dec1d3685cf317147458f
Tahmina <233046060+tahminaakter163001-dev@users.noreply.github.com>
2026-09-18 21:31:52 +0600

    Add responsive dashboard

--- src/main/java/com/example/projectmvc/controller/LoginController.java
+++ src/main/java/com/example/projectmvc/controller/LoginController.java
@@ -1,18 +1,57 @@
 package com.example.projectmvc.controller;
 
-    import javafx.fxml.FXML;
-    import javafx.scene.control.PasswordField;
-    import javafx.scene.control.TextField;
-    public class LoginController {
-        @FXML
-        private TextField usernameField;
-        @FXML
-        private PasswordField passwordField;
-        @FXML
-        private void handleLogin() {
-            String username = usernameField.getText();
-            String password = passwordField.getText();
-            System.out.println("Username: " + username);
-            System.out.println("Password: " + password);
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginController {
+
+    @FXML
+    private TextField usernameField;
+
+    @FXML
+    private PasswordField passwordField;
+
+    @FXML
+    private void handleLogin(ActionEvent event) throws Exception {
+
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+
+        if (username.equals("admin") && password.equals("1234")) {
+
+            FXMLLoader loader = new FXMLLoader(
+                    getClass().getResource(
+                            "/com/example/projectmvc/view/dashboard-view.fxml"
+                    )
+            );
+
+            Parent dashboard = loader.load();
+
+            Stage stage = (Stage) ((Node) event.getSource())
+                    .getScene()
+                    .getWindow();
+
+            Scene scene = new Scene(dashboard);
+
+            stage.setScene(scene);
+            stage.setTitle("CoffeeFlow - Dashboard");
+            stage.show();
+
+        } else {
+
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setHeaderText(null);
+            alert.setContentText("Invalid username or password.");
+            alert.showAndWait();
         }
     }
+}
