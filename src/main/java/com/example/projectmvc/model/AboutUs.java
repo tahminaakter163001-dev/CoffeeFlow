e8a3f147793a3a47a177718076baff7bc1ed9787
Tahmina <233046060+tahminaakter163001-dev@users.noreply.github.com>
2026-09-25 20:04:05 +0600

    Add About Us JSON data

---
+++ src/main/java/com/example/projectmvc/model/AboutUs.java
@@ -0,0 +1,25 @@
+package com.example.projectmvc.model;
+
+public class AboutUs {
+
+    private String title;
+    private String description;
+    private String version;
+    private String contact;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+}
